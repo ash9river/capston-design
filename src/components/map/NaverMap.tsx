465d517ab458ac1de5f4ed2f1ed9662cead0da7a
ash9river <ash9river@gmail.com>
2024-05-01 15:57:15 +0900

    fix: 네이버 맵 callback ref로 변경

--- src/components/map/NaverMap.tsx
+++ src/components/map/NaverMap.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useCallback } from 'react';
 
 import getLocation from 'hooks/getLocation';
 import styles from './NaverMap.module.scss';
@@ -8,7 +8,7 @@ function NaverMap() {
 
   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const mapRef = useCallback<React.RefCallback<HTMLDivElement>>(() => {
     if (typeof location !== 'string') {
       const map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(location.latitude, location.longitude),
@@ -28,7 +28,27 @@ function NaverMap() {
     }
   }, [location]);
 
-  return <div id="map" ref={ref} className={styles['map-container']} />;
+  /*   useEffect(() => {
+    if (typeof location !== 'string') {
+      const map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(location.latitude, location.longitude),
+      });
+
+      const currentMarker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(location.latitude, location.longitude),
+        map,
+        // 원하는 이미지로 마커 커스텀
+        // icon: {
+        //     url: pinImage,
+        //     size: new naver.maps.Size(50, 52),
+        //     origin: new naver.maps.Point(0, 0),
+        //     anchor: new naver.maps.Point(25, 26),
+        //   },
+      });
+    }
+  }, [location]); */
+
+  return <div id="map" ref={mapRef} className={styles['map-container']} />;
 }
 
 export default NaverMap;
